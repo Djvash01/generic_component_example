34067f0b6ddc9ad7c74a61e1245292a2e3eaf946
David Yepes <djvash01@gmail.com>
2022-08-24 09:12:22 -0500

    elevated button and set theme

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AlertScreen extends StatelessWidget {
-   
   const AlertScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-         child: Text('AlertScreen'),
+        child: ElevatedButton(
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Text(
+              'Mostrar alerta',
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          onPressed: () {},
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.close),
+          onPressed: () {
+            Navigator.pop(context);
+          }),
     );
   }
 }
--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -4,26 +4,29 @@ class AppTheme {
   static final Color primary = Colors.red.shade400;
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
-    //color primario
-    primaryColor: primary,
+      //color primario
+      primaryColor: primary,
 
-    //appbar theme
-    appBarTheme: AppBarTheme(
-      color: primary, 
-      elevation: 0
-    ),
+      //appbar theme
+      appBarTheme: AppBarTheme(color: primary, elevation: 0),
 
-    //Text button theme
-    textButtonTheme: TextButtonThemeData(
-      style: TextButton.styleFrom( primary: primary )
-    )
-  );
+      //Text button theme
+      textButtonTheme:
+          TextButtonThemeData(style: TextButton.styleFrom(primary: primary)),
+
+      //floatingActionsButtons
+      floatingActionButtonTheme:
+          FloatingActionButtonThemeData(backgroundColor: primary),
+
+      //elevate botton
+      elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              primary: primary, shape: const StadiumBorder(), elevation: 0)));
 
   static final ThemeData darkTheme = ThemeData.dark().copyWith(
-    //color primario
-    primaryColor: primary,
+      //color primario
+      primaryColor: primary,
 
-    //appbar theme
-    appBarTheme: AppBarTheme(color: primary, elevation: 0)
-  );
+      //appbar theme
+      appBarTheme: AppBarTheme(color: primary, elevation: 0));
 }
